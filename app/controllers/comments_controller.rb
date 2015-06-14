b63780cad4eed0dcd24154475930d93aa4f44541
John DeSilva <desilvjo@umich.edu>
2015-06-14 16:42:40 -0400

    Add Comments rails controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,45 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = post.comments
+    render json: @comments
+  end
+
+  def show
+    @comment = post.comments.find(params[:id])
+    render json: @comment
+  end
+
+  def create
+    @comment = post.comments.build(comment_params)
+    if @comment.save
+      render json: @comment, status: 201
+    else
+      render json: @comment.errors, status: 422
+    end
+  end
+
+  def update
+    @comment = post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: 422
+    end
+  end
+
+  def destroy
+    @comment = post.comments.find(params[:id])
+    @comment.destroy
+    head :no_content, status: 204
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def post
+    Post.find(params[:post_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :posts, except: [:new, :edit]
+  resources :posts, except: [:new, :edit] do
+    resources :comments, except: [:new, :edit]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
